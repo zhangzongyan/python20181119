03ed65054b182adfe53b8a93c8cde96d271bbccf
Zhangzongyan <zhangzongyan@uplooking>
2018-12-07 12:08:33 +0800

    字符串补充

--- base/day03/task.py
+++ base/day03/task.py
@@ -7,11 +7,17 @@ word = random.choice(words)
 
 # 'palep'
 save = word
+'''
 rand_str = ''
 while word: # 不是空串
 	rand_ind = random.randrange(len(word)) # random.choice(range(len(word)))
 	rand_str += word[rand_ind]	
 	word = word[:rand_ind]+word[rand_ind+1:] # 切除抽取的成员
+'''
+listword = list(word)
+random.shuffle(listword)
+rand_str = ''.join(listword)
+
 
 print('请猜测%s是什么单词, 你有三次机会:' % rand_str)
 
